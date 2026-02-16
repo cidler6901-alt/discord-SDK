afccad9a4c8236c589477ebce7d8354550cf3d09
Carson <carsonbro640@gmail.com>
2026-02-15 21:04:11 -0700

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let users = {}; // temp DB (we'll upgrade later)
+
+app.get("/", (req, res) => {
+  res.send("discord-SCK API online");
+});
+
+app.post("/link", (req, res) => {
+  const { discordId, webId } = req.body;
+  users[discordId] = { webId, profile: {} };
+  res.json({ success: true });
+});
+
+app.get("/users/:id", (req, res) => {
+  res.json(users[req.params.id] || null);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("API running on", PORT));
