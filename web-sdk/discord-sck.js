3852a362eaa80df0ac14f281149a6c6be6d77308
Carson <carsonbro640@gmail.com>
2026-02-15 23:26:32 -0700

    Update discord-sck.js

--- web-sdk/discord-sck.js
+++ web-sdk/discord-sck.js
@@ -2,12 +2,9 @@
 export class DiscordSCK {
   constructor({ clientId, api }) {
     this.clientId = clientId;
-    this.api = api; // e.g., "https://discord-sdk.onrender.com"
+    this.api = api;
   }
 
-  /**
-   * Login with Discord
-   */
   login() {
     const REDIRECT_URI = encodeURIComponent(`${this.api}/link-oauth`);
     const SCOPE = "identify";
@@ -15,20 +12,15 @@ export class DiscordSCK {
 
     const oauthUrl = `https://discord.com/api/oauth2/authorize?client_id=${this.clientId}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-    // Open Discord OAuth2 page in new tab
-    window.open(oauthUrl, "_blank");
+    const popup = window.open(oauthUrl, "_blank", "width=500,height=700");
+    if (!popup) alert("Please allow popups to login with Discord");
   }
 
-  /**
-   * Check for code returned from bot
-   * Call this on page load if you expect Discord to redirect back
-   */
   async handleRedirect() {
     const params = new URLSearchParams(window.location.search);
     const code = params.get("code");
     if (!code) return null;
 
-    // Send code to bot API
     const res = await fetch(`${this.api}/link-oauth`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -36,8 +28,6 @@ export class DiscordSCK {
     });
 
     if (!res.ok) throw new Error("Failed to link Discord account");
-    const data = await res.json();
-
-    return data;
+    return await res.json();
   }
 }
