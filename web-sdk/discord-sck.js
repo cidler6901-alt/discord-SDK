a99bf499bc4873a49b0fbc2c84c635806f03d65f
Carson <carsonbro640@gmail.com>
2026-02-16 01:10:28 -0700

    Update discord-sck.js

--- web-sdk/discord-sck.js
+++ web-sdk/discord-sck.js
@@ -1,5 +1,4 @@
 // web-sdk/discord-sck.js
-
 export class DiscordSCK {
   constructor({ clientId, api }) {
     this.clientId = clientId;
@@ -15,18 +14,16 @@ export class DiscordSCK {
 
     const oauthUrl = `https://discord.com/api/oauth2/authorize?client_id=${this.clientId}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-    // Open popup
     const popup = window.open(oauthUrl, "Discord Login", "width=500,height=700");
     if (!popup) return alert("Please allow popups to login with Discord");
 
     return new Promise((resolve, reject) => {
       const messageHandler = async (event) => {
-        // Only accept messages from your frontend page
         if (event.origin !== "https://cidler6901-alt.github.io") return;
         window.removeEventListener("message", messageHandler);
 
         const code = event.data?.code;
-        if (!code) return reject("No code received");
+        if (!code) return reject("No code received from Discord");
 
         try {
           const res = await fetch(`${this.api}/link-oauth`, {
